b1474d8d082e81be0a456e79cb89ed1dabe6db3b
TatevAghasaryan <tatevikaghasaryan@gmail.com>
2015-10-16 15:12:13 +0400

    Some changes

--- LINQDictionary/LINQDictionary/Program.cs
+++ LINQDictionary/LINQDictionary/Program.cs
@@ -10,6 +10,13 @@ namespace LINQDictionary
     {
         static void Main(string[] args)
         {
+            Dictionary<int, int> dic = new Dictionary<int, int>() { { 2, 3 }, { 4, 5 }, { 6, 7 }, { 8, 9 } };
+           // var query = dic.Where(c => c.Key%2==0);
+           //or  Dictionary<int,int> query = dic.Where(c => c.Key%2 == 0).ToDictionary(c => c.Key, c => c.Value);
+            var query = dic.OrderBy(c => c.Key > 5).ThenBy(c => c.Value>7);
+
+            
+            Console.ReadKey();
         }
     }
 }
