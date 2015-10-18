b719337fdb8c2987af668fdfb31f681b9cf2f62d
TatevAghasaryan <tatevikaghasaryan@gmail.com>
2015-10-19 01:04:53 +0400

    It works

--- WebTemperatureJQueryProject/WebTemperatureJQueryProject/app.js
+++ WebTemperatureJQueryProject/WebTemperatureJQueryProject/app.js
@@ -12,14 +12,21 @@
 
         })
     });
-    var obj = $("#capitals option:selected").text();
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + obj + "&appid=bd82977b86bf27fb59a04b61b657fb6f";
+    
 
     $("#get").click(function () {
+        var obj = $("#capitals option:selected").text();
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + obj + "&appid=bd82977b86bf27fb59a04b61b657fb6f";
         $.getJSON(url, function (data) {
-
-            console.log(data);
-
+            $('#name').text(data.name);
+            $('#coordLat').text(data.coord.lat);
+            $('#coordLon').text(data.coord.lon);
+            $('#mainHumidity').text(data.main.humidity);
+            $('#mainPressure').text(data.main.pressure);
+            $('#mainTemp').text(data.main.temp);
+            $('#mainTempMax').text(data.main.temp_max);
+            $('#mainTempMin').text(data.main.temp_min);
+            
         })
     });
 });
