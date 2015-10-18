f1c7d57c7f9983ed8551ae699f834d171513fa48
TatevAghasaryan <tatevikaghasaryan@gmail.com>
2015-10-18 23:39:22 +0400

    Failed2

--- WebTemperatureJQueryProject/WebTemperatureJQueryProject/app.js
+++ WebTemperatureJQueryProject/WebTemperatureJQueryProject/app.js
@@ -1,90 +1,31 @@
 ﻿$(function () {
-    
- //   $.getJSON('https://freegeoip.net/json/')
- //    .done(function (location) {
- //        $('#country').html(location.country_name);
- //        $('#country_code').html(location.country_code);
- //        $('#region').html(location.region_name);
- //        $('#region_code').html(location.region_code);
- //        $('#city').html(location.city);
- //        $('#latitude').html(location.latitude);
- //        $('#longitude').html(location.longitude);
- //        $('#timezone').html(location.time_zone);
- //        $('#ip').html(location.ip);
-    //});
 
+    //var url = "http://api.openweathermap.org/data/2.5/weather?q=Yerevan&appid=bd82977b86bf27fb59a04b61b657fb6f";
+    //var a = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json";
+    var urll = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json";
 
-    $.getJSON('https://raw.githubusercontent.com/mledoze/countries/master/countries.json')
-     .done(function (location) {
-         $('#country').html(location.country_name);
-         $('#country_code').html(location.country_code);
-         $('#region').html(location.region_name);
-         $('#region_code').html(location.region_code);
-         $('#city').html(location.capital);
-         $('#latitude').html(location.latitude);
-         $('#longitude').html(location.longitude);
-         $('#timezone').html(location.time_zone);
-         $('#ip').html(location.ip);
-     });
+    console.log(typeof (a));
+    $.getJSON(urll, function (data) {
+        $.each(data, function (i, data) {
+            $('#capitals').append($('<option>').text(data.capital).attr('value', data.capital));
+            console.log(data.capital);
+
+        })
+    });
+    var obj = $("#capitals option:selected").text();
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + obj + "&appid=bd82977b86bf27fb59a04b61b657fb6f";
+
+    $("#get").click(function () {
+        $.getJSON(url, function (data) {
+
+            console.log(data);
+
+        })
+    });
 });
 
 
 
 
-
-//$(function () {
-//    alert("Your location is: " + geoplugin_countryName() + ", " + geoplugin_region() + ", " + geoplugin_city());
-
-//    var country = geoplugin_countryName();
-//    $("#country").append("<option value='1' selected>" + country + "</option>");
-
-//    var zone = geoplugin_region();
-//    $("#zone").append("<option value='1' selected>" + zone + "</option>");
-
-//    var district = geoplugin_city();
-//    $("#district").append("<option value='1' selected>" + district + "</option>");
-//});
-
-
-
-
-
-
-//$.ajax({
-//    url: 'https://raw.githubusercontent.com/mledoze/countries/master/countries.json',
-//})
-//.done(function (data) {
-    
-//    data.filter(function (data) {
-//        return data.area >= 30000;
-//    }).forEach(function (s) {
-//        console.log(s);
-//    })
-//})
-//.fail(function () {
-//    alert("Ajax failed to fetch data")
-//})
-
-//$(function () {
-//    var url = "http://api.openweathermap.org/data/2.5/weather?q=Yerevan&appid=bd82977b86bf27fb59a04b61b657fb6f";
-//    var $container = $('.container');
-//    var $result = $('<textarea>').addClass('result');
-//    var urlFinal = url.split('=', '&');
-//    var urlFi = urlFinal.split('&');
-//    console.log(urlFi);
-//    $.getJSON(url, function (data) {
-//        var $res = data.capital;
-//        $("#find").click(function () {
-//            var text = $("#comment").val();
-//            var $newItem = $result.clone();
-//            $newItem.html(text);
-//            $newItem.hide();
-//            $container.append($newItem);
-//            $newItem.show(500);
-//        });
-//    });
-    
-
-//});
 
 
