793c093b59ee5b7a3cbbee2ea242b8d1e0b88d98
TatevAghasaryan <tatevikaghasaryan@gmail.com>
2015-10-19 20:20:58 +0400

    Update

--- WebTemperatureJQueryProject/WebTemperatureJQueryProject/app.js
+++ WebTemperatureJQueryProject/WebTemperatureJQueryProject/app.js
@@ -1,21 +1,14 @@
 ﻿$(function () {
-
-    //var url = "http://api.openweathermap.org/data/2.5/weather?q=Yerevan&appid=bd82977b86bf27fb59a04b61b657fb6f";
-    //var a = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json";
     var urll = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json";
-
-    console.log(typeof (a));
     $.getJSON(urll, function (data) {
         $.each(data, function (i, data) {
-            $('#capitals').append($('<option>').text(data.capital).attr('value', data.capital));
-            console.log(data.capital);
-
+            $('#capitals').append($('<option>').text(data.capital).attr('value', data.capital));            
         })
     });
     
 
     $("#get").click(function () {
-        var obj = $("#capitals option:selected").text();
+        var obj = $("#capitals option:selected").val();
         var url = "http://api.openweathermap.org/data/2.5/weather?q=" + obj + "&appid=bd82977b86bf27fb59a04b61b657fb6f";
         $.getJSON(url, function (data) {
             $('#name').text(data.name);
