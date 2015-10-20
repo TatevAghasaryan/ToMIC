46eeaf1258357c728c34ca2b723c7ba993bd5fc3
TatevAghasaryan <tatevikaghasaryan@gmail.com>
2015-10-21 00:04:34 +0400

    Initial-half version

---
+++ Events/Events/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class Printer
+    {
+        public event EventHandler PrintStarted;
+        public event EventHandler Printing;
+        public event EventHandler PrintFinished;
+
+
+    }
+}
